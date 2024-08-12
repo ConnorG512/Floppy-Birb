8dcd656efe8799c0a2f5bce17da8aa7406bf4107
Connor Garey <connorgarey1999@gmail.com>
2024-08-12 23:15:20 +0100

    Character now consistantly falls. Pressing Space will make him fly up.

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -2,7 +2,11 @@ extends CharacterBody2D
 
 var bAlive = true
 var CharacterBody = CharacterBody2D
-@export var descent_speed = 250
+
+@onready var JumpTimer = $JumpTimer
+
+@export var descent_speed = 450
+@export var pull_up_power = -800
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,10 +14,17 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
-	velocity.y = descent_speed
-	move_and_slide()
+func _process(delta):
+	movement()
 	
 
-func pull_down():
+func _physics_process(delta):
 	pass
+func movement():
+	
+	velocity.y = descent_speed
+	
+	if Input.is_action_pressed("pull_up"):
+		velocity.y = pull_up_power
+		
+	move_and_slide()
