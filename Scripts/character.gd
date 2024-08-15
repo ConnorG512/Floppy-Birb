aac6dde28e6e40411b768a173fd1bb44934aaa9b
Connor Garey <connorgarey1999@gmail.com>
2024-08-15 12:03:52 +0100

    Rewrote the character jumping. Character will now fly up when space is pressed.

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -5,26 +5,31 @@ var CharacterBody = CharacterBody2D
 
 @onready var JumpTimer = $JumpTimer
 
-@export var descent_speed = 450
-@export var pull_up_power = -800
+@export var character_speed = 450
+
+var is_dead = false
+var can_jump = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	movement()
-	
+	pass
 
 func _physics_process(delta):
+	character_movement(character_speed)
 	pass
-func movement():
+
+func character_movement(character_speed):
 	
-	velocity.y = descent_speed
-	
-	if Input.is_action_pressed("pull_up"):
-		velocity.y = pull_up_power
+	if is_dead == true:
+		return
+	else: 
+		velocity.y = character_speed
+		move_and_slide()
 		
-	move_and_slide()
+		if Input.is_action_pressed("pull_up"):
+			velocity.y = -character_speed * 2.5
+			move_and_slide()
