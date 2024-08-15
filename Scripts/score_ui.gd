8c8a2730300a619722e0e748b18bb8d9ff1780ce
Connor Garey <connorgarey1999@gmail.com>
2024-08-15 19:45:54 +0100

    Added score UI to level.

--- Scripts/score_ui.gd
+++ Scripts/score_ui.gd
@@ -4,6 +4,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	SignalEventBus.game_score_updated.connect(update_score)
 	pass # Replace with function body.
 
 
