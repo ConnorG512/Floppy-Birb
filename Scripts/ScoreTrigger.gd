40863f2b5e36d1db8e7a2a6712c604e2f6f74f63
Connor Garey <connorgarey1999@gmail.com>
2024-08-15 19:20:47 +0100

    Added moving pipes.

--- Scripts/ScoreTrigger.gd
+++ Scripts/ScoreTrigger.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
 var score_amount = 1
+var can_be_triggered = true
 
 func score_trigger_entered(body):
-	print(body)
-	SignalEventBus.emit_signal("game_score_updated")
+	if can_be_triggered == true:
+		SignalEventBus.emit_signal("game_score_updated")
+		can_be_triggered = false
+	
---
+++ Scripts/pipe_spawn.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+var movespeed = -400
+
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	generate_speed()
+	move_accross_screen()
+	pass
+
+func generate_speed():
+	# Generate a random movespeed for speed variety
+	movespeed = randi_range(-1000, -100)
+	pass
+	
+func move_accross_screen():
+	velocity.x = movespeed 
+	move_and_slide()
+
+func spawn_pipe():
+	pass
