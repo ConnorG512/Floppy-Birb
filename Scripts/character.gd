62c60916590811b3bca8f98f7ea303f6885a805b
Connor Garey <connorgarey1999@gmail.com>
2024-08-15 14:18:38 +0100

    Added score to character.

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -9,6 +9,7 @@ var CharacterBody = CharacterBody2D
 
 var is_dead = false
 var can_jump = true
+var current_score = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
