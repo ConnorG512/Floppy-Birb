93e348180bfb3dd413e0c1bd5fbda7b85695ee3b
Connor Garey <connorgarey1999@gmail.com>
2024-08-16 00:47:00 +0100

    Added random spawn time for the pipes.

--- Scripts/pipe_spawn_manager.gd
+++ Scripts/pipe_spawn_manager.gd
@@ -5,13 +5,17 @@ var pipe_spawn_pos = Vector2(720,0)
 
 @onready var spawn_timer = $Timer
 
+func timer_change_wait_time():
+	var spawn_time = randf_range(0.5, 3.0)
+	spawn_timer.wait_time = spawn_time
+
 func spawn_new_pipe():
 	var pipe_spawn_instance = pipe_spawn.instantiate()
 	pipe_spawn_instance.position = pipe_spawn_pos
 	add_child(pipe_spawn_instance)
+	timer_change_wait_time()
 	spawn_timer.start()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	spawn_new_pipe()
-
