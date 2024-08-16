9e8989b1dcb215c7f2b1ef7d296c93b245ec4f26
Connor Garey <connorgarey1999@gmail.com>
2024-08-16 12:49:36 +0100

    Added base for RNG pipe spawning.

--- Scripts/pipe_spawn_manager.gd
+++ Scripts/pipe_spawn_manager.gd
@@ -1,16 +1,31 @@
 extends Node2D
 
-var pipe_spawn = preload("res://Scenes/Pipe Spawn/pipe_spawn.tscn")
-var pipe_spawn_pos = Vector2(720,0)
-
 @onready var spawn_timer = $Timer
 
 func timer_change_wait_time():
 	var spawn_time = randf_range(0.5, 3.0)
 	spawn_timer.wait_time = spawn_time
 
-func spawn_new_pipe():
-	var pipe_spawn_instance = pipe_spawn.instantiate()
+func pipe_spawn_rng():
+	var pipe_spawn
+	var pipe_spawn_instance
+	
+	var pipe_spawn_num = randi_range(0,1)
+	
+	match pipe_spawn_num:
+		0:
+			pipe_spawn = preload("res://Scenes/Pipe Spawn/pipe_spawn.tscn")
+			pipe_spawn_instance = pipe_spawn.instantiate()
+			place_pipe_in_world(pipe_spawn_instance)
+		1:
+			pipe_spawn = preload("res://Scenes/Pipe Spawn/pipe_spawn_2.tscn")
+			pipe_spawn_instance = pipe_spawn.instantiate()
+			place_pipe_in_world(pipe_spawn_instance)
+		_:
+			print("Invalid number")
+
+func place_pipe_in_world(pipe_spawn_instance):
+	var pipe_spawn_pos = Vector2(720,0)
 	pipe_spawn_instance.position = pipe_spawn_pos
 	add_child(pipe_spawn_instance)
 	timer_change_wait_time()
@@ -18,4 +33,4 @@ func spawn_new_pipe():
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	spawn_new_pipe()
+	pipe_spawn_rng()
